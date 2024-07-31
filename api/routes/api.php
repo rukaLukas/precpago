4da557ebc05d5d35d34e35e50a5fb152cd4872d4
Lucas <lukas.figueiredo@hotmail.com>
2024-07-31 13:01:04 -0300

    Configuração docker

--- api/routes/api.php
+++ api/routes/api.php
@@ -14,6 +14,14 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+// Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+//     return $request->user();
+// });
+
+// Route::get('/health', function () {
+//     return ['up' => true];
+// });
+
+Route::get('/health', function(){    
+    return response()->json(['status' => 'ok']);
 });
